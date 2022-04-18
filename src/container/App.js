b8af33f5139d0e1c41b857433d09350bcedd4621
paleksanderr <pawel.aleksander@gmail.com>
2022-04-18 14:48:27 +0200

    git

--- src/container/App.js
+++ src/container/App.js
@@ -5,24 +5,16 @@ import Navigation from '../components/Navigation/Navigation';
 import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
 import Rank from '../components/Rank/Rank';
-import Particles from "react-particles-js";
+import Tlo from '../components/tlo/tlo';
 
 
-const particlesOptions = {
-  particles: {
-    number: { value: 60, 
-      density: { 
-      enable: true, 
-      value_area: 800 } },
-  },
-};
 class App extends Component {
+  
   render() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particlesOptions}
-      />
+
+    return (
+      <div className="App">
+      <Tlo />
       <Navigation />
       <Logo />
       <Rank />
