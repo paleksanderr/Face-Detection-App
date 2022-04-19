7457fe811c0265d571aec088ce88b7ae6f74f730
paleksanderr <pawel.aleksander@gmail.com>
2022-04-19 14:29:59 +0200

    add

--- src/container/App.js
+++ src/container/App.js
@@ -1,15 +1,43 @@
 
 import './App.css';
 import React, { Component } from 'react';
+import Clarifai from "clarifai";
 import Navigation from '../components/Navigation/Navigation';
 import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
 import Rank from '../components/Rank/Rank';
 import Tlo from '../components/tlo/tlo';
 
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: "0b13020848bb4a6fb2b83895d1e7a5b5",
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      input: '',}
+    }
+
+    onInputChange = (event) => {
+      console.log(event.target.value);
+      // this.setState({input: event.target.value});
+    }
+    onSubmit = () => {
+      console.log('submit');
+    }
   
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
 
     return (
@@ -18,7 +46,9 @@ class App extends Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onSubmit={this.onSubmit}/>
       {/* <FaceRecognition /> */}
     </div>
   );
