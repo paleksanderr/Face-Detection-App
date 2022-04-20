8755141ef41b58262e38c7ee144aba2f5fec2536
paleksanderr <pawel.aleksander@gmail.com>
2022-04-20 17:29:27 +0200

    progress

--- src/container/App.js
+++ src/container/App.js
@@ -20,29 +20,36 @@ class App extends Component {
     this.state = {
       input: "",
       imageUrl: "",
+      box: {},
     };
+  }
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    }
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
   }
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   };
   onSubmit = () => {
-   this.setState({ imageUrl: this.state.input });
-   app.models
-     .predict(
-       Clarifai.FACE_DETECT_MODEL,
-       this.state.input
-     )
-     .then(
-       function (response) {
-         console.log(
-           response.outputs[0].data.regions[0].region_info.bounding_box
-         );
-       },
-       function (err) {
-         console.log(err);
-       }
-     );
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   };
 
   render() {
@@ -52,8 +59,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
-        <Face imageUrl={this.state.imageUrl} />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onSubmit}
+        />
+        <Face box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
